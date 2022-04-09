bdf7f41a696c800e2ea23be755333ac651130281
unknown <musab@point-star.com>
2022-04-09 23:10:02 +0500

    error removed

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
 const express = require('express')
 const path = require('path')
@@ -28,7 +25,7 @@ const userid = 1603
 app.use(express.urlencoded({ extended: true }))
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, './views'))
+app.set('views', path.join(__dirname, './views')) 
 app.use('/', express.static(path.join(__dirname, './public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
@@ -38,7 +35,10 @@ app.use(methodOverride('_method'))
 
 
 app.get('/',(req,res)=>{
-  res.render("index")
+  breadcrumbs = {"noBreadcrumbs" : {name:"",link:""}};
+  let route = "partials/_content"
+
+  res.render('index',{route,breadcrumbs})
 })
 
 const port = process.env.PORT || 3000
