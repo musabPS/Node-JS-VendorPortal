c855177401d9e7da48ad229a2fc56883fa1787e5
ACCOUNT MAIL SERVICES <jasimarifali@gmail.com>
2022-04-11 15:39:30 +0500

    changes

--- models/mongoose.js
+++ models/mongoose.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 const validator= require('validator')
 const mongodbLocal = 'mongodb://127.0.0.1:27017/vendor-portal-app'
 //const mongodbGcp =  'mongodb://jasim:jasim123@35.240.200.56:27017/admin'
+const mongodbAtlas = 'mongodb+srv://pointstar:poinstar123@cluster0.nenzn.mongodb.net/vendor-portal-app?retryWrites=true&w=majority'
 
-mongoose.connect( mongodbLocal ,{
+mongoose.connect( mongodbAtlas ,{
     useNewUrlParser:true,
 })
 var db = mongoose.connection
