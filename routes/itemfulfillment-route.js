efcffec9f5590827ad9d72013eb318a8fa0fbd28
unknown <musab@point-star.com>
2022-04-19 15:00:48 +0500

    itemfulfilmentList and view

--- routes/itemfulfillment-route.js
+++ routes/itemfulfillment-route.js
@@ -41,7 +41,7 @@ router.get('/itemFulfillmentList', async (req, res) => {
 
     var data = []
     try {
-        data =  await ItemFulfillments.find({})
+        data =  await ItemFulfillments.find({vendorInternalId:req.session.user_id}) 
         // data = await ItemFulfillments.findOne({internalId:"6728"})
         console.log("check",data)
         //data.purchaseRequests = purchaseRequests
@@ -58,22 +58,76 @@ router.get('/itemFulfillmentList', async (req, res) => {
 
 })
 
-router.get('/itemfulfillmentForm', (req, res) => {
+router.get('/itemfulfillmentForm&id=:id',async (req, res) => {
+
+
+    var { id } = req.params
+    var data = []
+    var query = { internalId: id };
+    try {
+        console.log("query", query)
+        data = await ItemFulfillments.findOne(query)
+        console.log("data", data)
+
+        var tranId   = data["ifNumber"]
+        var date     = moment(data["date"]).format("MM-DD-YYYY")
+        var totalQty = data["quantity"]
+        var location = data["location"]
+        var totalAmount  = 0 
+
+        //data.purchaseRequests = purchaseRequests
+    }
+    catch (e) {
+        console.log(e)
+    }
+
     let route = "pages/itemFulfillmentForm"
     let listName = "Purchase Request"
 
 
-    axios.get('https://tstdrv925863.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=700&deploy=1&compid=TSTDRV925863&h=dfb1a0d8daae184c8cff&type=itemRecipt', {
-        firstName: 'Fred',
-        lastName: 'Flintstone'
+    // axios.get('https://tstdrv925863.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=700&deploy=1&compid=TSTDRV925863&h=dfb1a0d8daae184c8cff&type=itemRecipt', {
+    //     firstName: 'Fred',
+    //     lastName: 'Flintstone'
+    // })
+    //     .then(function (response) {
+    //         console.log(response.data[0]);
+
+    //         let tableData = response.data
+
+    //         breadcrumbs = { "noBreadcrumbs": { name: "", link: "" } };
+    //         res.render('index', { route, listName, breadcrumbs, tableData })
+    //     })
+    //     .catch(function (error) {
+    //         console.log("erorr", error);
+       // });
+
+       breadcrumbs = { "noBreadcrumbs": { name: "", link: "" } };
+       res.render('index', { route, listName, breadcrumbs,tranId,date,totalQty,totalAmount,location})
+
+})
+
+router.get('/itemfulfillmentForm/itemdetail&id=:id',(req, res) => {
+
+    console.log("chddd", req.params)
+
+
+    let route = "pages/purchaseRequestForm"
+    let listName = "Purchase Request"
+    var { id } = req.params
+
+    console.log("chd", id)
+    axios.get('https://tstdrv925863.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=700&deploy=1&compid=TSTDRV925863&h=dfb1a0d8daae184c8cff&type=itemRecipt&internalid=' + id, {
     })
         .then(function (response) {
-            console.log(response.data[0]);
+            // console.log(response.data[0]);
 
             let tableData = response.data
-
+            let tranId = response.data[0].values["GROUP(tranid)"]
+            let location = response.data[0].values["GROUP(locationnohierarchy)"]
+            let date = response.data[0].values["GROUP(trandate)"]
             breadcrumbs = { "noBreadcrumbs": { name: "", link: "" } };
-            res.render('index', { route, listName, breadcrumbs, tableData })
+            res.send(tableData)
+            //  res.render('index', {route,listName ,breadcrumbs,tableData,tranId,location,date}) 
         })
         .catch(function (error) {
             console.log("erorr", error);
